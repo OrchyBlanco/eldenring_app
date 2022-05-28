0c0f4c26ebad4349c5c4f9088f8aad6e926c5110
Carleussox <95582234+Carleussox@users.noreply.github.com>
2022-05-28 22:42:21 +0100

    Carta de ID creada
    Falta modificar tamaño para que quede más estético

--- pages/categories/[endpoint]/[id].tsx
+++ pages/categories/[endpoint]/[id].tsx
@@ -1,28 +1,78 @@
-
-import { Container, Grid, Typography } from "@mui/material";
+import {
+  Container,
+  Grid,
+  Typography,
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions,
+} from "@mui/material";
 import { useRouter } from "next/router";
-export default function Element({serverData}:any) {
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import { Box } from "@mui/system";
 
-    const data=serverData.data
-    
-    return (
-      <Container maxWidth="xl">
-       <Grid container spacing={{xs:1,md:2}} columns={{xs:2,md:12}}>
+export default function Element({ serverData }: any) {
+  const data = serverData.data;
+
+  return (
+    <Container maxWidth="xl"
+    sx={{
+      display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+    }}>
+      {/* <Grid container spacing={{xs:1,md:2}} columns={{xs:2,md:12}}>
          <Grid item>
               <Typography> {data.name}</Typography>
          </Grid>
-       </Grid>
-      </Container>
-    );
-  }
-  export async function getServerSideProps(context: any) {
-    const endpoint = context.query.endpoint;
-    const id = context.query.id;
-    
-    const response = await fetch(
-      `https://eldenring.fanapis.com/api/${endpoint}/${id}`    );
-    const serverData = await response.json();
-  
-    return { props: { serverData } };
-  }
-  
+       </Grid> */}
+
+      <Card
+        sx={{
+          maxWidth: 800,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Box sx={{
+              display: { xs: "none", md: "flex" },
+              width:{ xs:"100%",md:"80%",lg:"50%"},
+            }}>
+        <CardMedia
+          component="img"
+          height="240"
+          
+          image={`${data.image}`}
+          alt="green iguana"
+        />
+        </Box>
+        
+        <CardContent>
+          <Typography gutterBottom variant="h4" component="div">
+            {data.name}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div">
+            {data.category}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {data.description}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Container>
+  );
+}
+export async function getServerSideProps(context: any) {
+  const endpoint = context.query.endpoint;
+  const id = context.query.id;
+
+  const response = await fetch(
+    `https://eldenring.fanapis.com/api/${endpoint}/${id}`
+  );
+  const serverData = await response.json();
+
+  return { props: { serverData } };
+}
--- pages/categories/[endpoint]/index.tsx
+++ pages/categories/[endpoint]/index.tsx
@@ -13,12 +13,18 @@ import { useRouter } from "next/router";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import genericImage from "../../../public/images/generic.jpg";
+<<<<<<< Updated upstream
+=======
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { ThemeContext } from "@emotion/react";
+>>>>>>> Stashed changes
 
 export default function Categories({ serverData }: any) {
   const router = useRouter();
 
   return (
     <>
+<<<<<<< Updated upstream
       <Container maxWidth="xl">
         <Grid container spacing="2">
           <Grid item xs={12}>
@@ -37,6 +43,65 @@ export default function Categories({ serverData }: any) {
                 >
                   <h1>{router.query.endpoint}</h1>
                 </ListSubheader>
+=======
+      <ListSubheader
+        component="div"
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "90%",
+          textTransform: "capitalize",
+        }}
+      >
+        <h1>{router.query.endpoint}</h1>
+      </ListSubheader>
+      <Box
+        sx={{
+          height: 450,
+          width: "90%",
+          display: "grid",
+          gap: 2,
+          gridTemplateColumns: {
+            xs: "repeat(2, 1fr)",
+            sm: "repeat(3, 1fr)",
+            md: "repeat(4, 1fr)",
+            lg: "repeat(5, 1fr)",
+            xl: "repeat(7, 1fr)",
+          },
+          [`& .${imageListItemClasses.root}`]: {
+            display: "flex",
+            flexDirection: "column",
+          },
+        }}
+      >
+        {serverData.data.map((item: any, index: number) => (
+          <Link
+            href={"/categories/[endpoint]/[id]"}
+            as={`/categories/${router.query.endpoint}/${item.id}`}
+          >
+            <a>
+              <ImageListItem key={index} cols={2}>
+                
+                <img
+                  src={`${item.image != null ? item.image : genericImage}`}
+                  srcSet={`${item.image}`}
+                  alt={item.name}
+                  loading="eager"
+                  color="background.paper"
+                />
+
+                <ImageListItemBar
+                  title={item.name}
+                  subtitle={
+                    item.category != undefined
+                      ? item.category
+                      : item.type != undefined
+                      ? item.type
+                      : item.affinity
+                  }
+                />
+>>>>>>> Stashed changes
               </ImageListItem>
               {serverData.data.map((item: any, index: number) => (
                 <Link
