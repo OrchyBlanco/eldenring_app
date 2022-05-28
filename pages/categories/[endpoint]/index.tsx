acb0ff685a696efdf6929d82ba7138e940dde344
Jesus Blanco Torres <18281857+OrchyBlanco@users.noreply.github.com>
2022-05-28 21:18:29 +0100

    Update Index categories

--- pages/categories/[endpoint]/index.tsx
+++ pages/categories/[endpoint]/index.tsx
@@ -1,81 +1,134 @@
 import {
   Button,
   Container,
+  Box,
   Grid,
   IconButton,
   ImageList,
   ImageListItem,
+  imageListItemClasses,
   ImageListItemBar,
   ListSubheader,
 } from "@mui/material";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import Image from "next/image";
+
 import { useEffect, useState } from "react";
 import genericImage from "../../../public/images/generic.jpg";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 export default function Categories({ serverData }: any) {
   const router = useRouter();
 
   return (
     <>
-      <Container maxWidth="xl">
-        <Grid container spacing="2">
-          <Grid item xs={12}>
-            <ImageList>
-              <ImageListItem key="Subheader" cols={12}>
-                <ListSubheader
-                  component="div"
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
+      {/*  <Container maxWidth="xl">
+        <ImageList cols={4}>
+          <ImageListItem key="Subheader" cols={4}>
+            <ListSubheader
+              component="div"
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
 
-                    textTransform: "capitalize",
+                textTransform: "capitalize",
+              }}
+            >
+              <h1>{router.query.endpoint}</h1>
+            </ListSubheader>
+          </ImageListItem>
+          {serverData.data.map((item: any, index: number) => (
+            <Link
+              href={"/categories/[endpoint]/[id]"}
+              as={`/categories/${router.query.endpoint}/${item.id}`}
+            >
+              <a>
+                <ImageListItem key={index} cols={2}>
+                  
 
-                  }}
-                >
-                  <h1>{router.query.endpoint}</h1>
-                </ListSubheader>
+                  <img
+                    src={`${item.image != null ? item.image : genericImage}`}
+                    srcSet={`${item.image}`}
+                    alt={item.name}
+                    loading="eager"
+                  />
+                  <ImageListItemBar
+                    title={item.name}
+                    subtitle={
+                      item.category != undefined
+                        ? item.category
+                        : item.type != undefined
+                        ? item.type
+                        : item.affinity
+                    }
+                  />
+                </ImageListItem>
+              </a>
+            </Link>
+          ))}
+        </ImageList>
+      </Container> */}
+      <ListSubheader
+        component="div"
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "90%",
+          textTransform: "capitalize",
+        }}
+      >
+        <h1>{router.query.endpoint}</h1>
+      </ListSubheader>
+      <Box
+        
+        sx={{
+          height: 450,
+          width: "90%",
+          display: "grid",
+          gap: 2,
+          gridTemplateColumns: {
+            xs: "repeat(2, 1fr)",
+            sm: "repeat(3, 1fr)",
+            md: "repeat(4, 1fr)",
+            lg: "repeat(5, 1fr)",
+            xl: "repeat(7, 1fr)",
+          },
+          [`& .${imageListItemClasses.root}`]: {
+            display: "flex",
+            flexDirection: "column",
+          },
+        }}
+      >
+        {serverData.data.map((item: any, index: number) => (
+          <Link
+            href={"/categories/[endpoint]/[id]"}
+            as={`/categories/${router.query.endpoint}/${item.id}`}
+          >
+            <a>
+              <ImageListItem key={index} cols={2}>
+                <img
+                  src={`${item.image != null ? item.image : genericImage}`}
+                  srcSet={`${item.image}`}
+                  alt={item.name}
+                  loading="eager"
+                />
+                <ImageListItemBar
+                  title={item.name}
+                  subtitle={
+                    item.category != undefined
+                      ? item.category
+                      : item.type != undefined
+                      ? item.type
+                      : item.affinity
+                  }
+                />
               </ImageListItem>
-              {serverData.data.map((item: any, index: number) => (
-                <Link
-                  href={"/categories/[endpoint]/[id]"}
-                  as={`/categories/${router.query.endpoint}/${item.id}`}
-                >
-                  <a>
-                    <ImageListItem key={index} cols={5}>
-
-                      {/* <Image src={item.image} alt={""} height={60} width={60} /> */}
-
-                      <img
-                        src={`${
-                        
-                          item.image != null ? item.image : genericImage
-                        }`}
-                        srcSet={`${item.image}`}
-                        alt={item.name}
-                        loading="eager"
-
-                      />
-                      <ImageListItemBar
-                        title={item.name}
-                        subtitle={
-                          item.category != undefined
-                            ? item.category
-                            : item.type != undefined
-                            ? item.type
-                            : item.affinity
-                        }
-                      />
-                    </ImageListItem>
-                  </a>
-                </Link>
-              ))}
-            </ImageList>
-          </Grid>
-        </Grid>
-      </Container>
+            </a>
+          </Link>
+        ))}
+      </Box>
     </>
   );
 }
